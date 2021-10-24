f6abee6c0613ca42e352df42c64dc866d8754d46
Guido Glielmi <guidoglielmi@gmail.com>
2021-10-24 01:12:55 -0300

    validators changed, validate everything on submit added

--- js/form.js
+++ js/form.js
@@ -1,51 +1,109 @@
 window.onload = () => {
-  for (let key in localStorage) {
-    if (document.querySelector(`input[name=${key}]`)) document.getElementById(key).value = localStorage[key];
+  for (let key of document.querySelectorAll(".subscribe input")) {
+    if (localStorage[key.name]) key.value = localStorage[key.name];
   }
+  formWelcomeSign.innerHTML = "HOLA " + formName.value;
 };
-function formError(e) {
-  let error;
-  let string = e.target.value;
-  if (e.target.id === "name") {
-    if (string.length < 6 || specialCharValidator(string, 1, " ")) {
-      nameError.innerHTML = error = "Ingrese Nombre y Apellido";
+let formName = document.getElementById("name");
+let formEmail = document.getElementById("email");
+let formPassword = document.getElementById("password");
+let formAge = document.getElementById("age");
+let formTelephone = document.getElementById("telephone");
+let formAddress = document.getElementById("address");
+let formCity = document.getElementById("city");
+let formZipCode = document.getElementById("zip-code");
+let formDni = document.getElementById("dni");
+
+let nameError = document.querySelector(".name-error");
+let emailError = document.querySelector(".email-error");
+let passwordError = document.querySelector(".password-error");
+let ageError = document.querySelector(".age-error");
+let telephoneError = document.querySelector(".tel-error");
+let addressError = document.querySelector(".address-error");
+let cityError = document.querySelector(".city-error");
+let zipCodeError = document.querySelector(".zip-code-error");
+let dniError = document.querySelector(".dni-error");
+
+let formWelcomeSign = document.getElementById("welcome-sign");
+let modalSign = document.getElementById("modal");
+let modalText = document.getElementById("modal-text");
+let modalClose = document.getElementById("close-button");
+let formInputs = Array.from(document.getElementsByClassName("input"));
+let submitButton = document.getElementById("submit");
+
+const errorFunctions = {
+  //validators should go in the HTML ORDER
+  nameValidator: ({ target: { value } }) => {
+    if (value.length < 6 || specialCharValidator(value, 1, " ")) {
+      return (nameError.innerHTML = "Ingrese Nombre y Apellido");
     }
-  } else if (e.target.id === "email") {
-    if (specialCharValidator(string, 1, "@", ".")) {
-      emailError.innerHTML = error = "Ingrese un formato de email válido";
+  },
+  emailValidator: ({ target: { value } }) => {
+    if (specialCharValidator(value, 1, "@", ".")) {
+      return (emailError.innerHTML = "Ingrese un formato de email válido");
     }
-  } else if (e.target.id === "password") {
-    if (string.length < 8 || numAndCharValidator(string)) {
-      passwordError.innerHTML = error = "La contraseña debe contener números y letras con mas de ocho caracteres ";
+  },
+  passwordValidator: ({ target: { value } }) => {
+    if (value < 8 || numAndCharValidator(value)) {
+      return (passwordError.innerHTML = "La contraseña debe contener números y letras con mas de ocho caracteres ");
     }
-  } else if (e.target.id === "age") {
-    if (string !== Math.round(string) && string < 18) {
+  },
+  ageValidator: ({ target: { value } }) => {
+    if (value !== Math.round(value) && value < 18) {
       // Math.round works with strings
-      ageError.innerHTML = error = "Ingrese un número entero mayor a 18";
+      return (ageError.innerHTML = "Ingrese un número entero mayor a 18");
     }
-  } else if (e.target.id === "telephone") {
-    if (string.length < 7 || isNaN(string)) {
-      telephoneError.innerHTML = error = "Ingrese 8 o más números sin espacios, paréntesis o guiones";
+  },
+  telephoneValidator: ({ target: { value } }) => {
+    if (value.length < 7 || isNaN(value)) {
+      return (telephoneError.innerHTML = "Ingrese 8 o más números sin espacios, paréntesis o guiones");
     }
-  } else if (e.target.id === "address") {
-    if (string.length < 5 || specialCharValidator(string, 1, " ") || numAndCharValidator(string)) {
-      addressError.innerHTML = error = "Ingrese una dirección y un número";
+  },
+  addressValidator: ({ target: { value } }) => {
+    if (value.length < 5 || specialCharValidator(value, 1, " ") || numAndCharValidator(value)) {
+      return (addressError.innerHTML = "Ingrese una dirección y un número");
     }
-  } else if (e.target.id === "city") {
-    if (string.length < 3) {
-      cityError.innerHTML = error = "Ingrese una ciudad válida";
+  },
+  cityValidator: ({ target: { value } }) => {
+    if (value.length < 3) {
+      return (cityError.innerHTML = "Ingrese una ciudad válida");
     }
-  } else if (e.target.id === "zip-code") {
-    if (string.length < 3) {
-      zipCodeError.innerHTML = error = "Ingrese un código postal válido";
+  },
+  zipCodeValidator: ({ target: { value } }) => {
+    if (value.length < 3) {
+      return (zipCodeError.innerHTML = "Ingrese un código postal válido");
     }
-  } else if (e.target.id === "dni") {
-    if ((string.length !== 7 && string.length !== 8) || isNaN(string)) {
-      dniError.innerHTML = error = "Ingrese un DNI válido";
+  },
+  dniValidator: ({ target: { value } }) => {
+    if ((value.length !== 7 && value.length !== 8) || isNaN(value)) {
+      return (dniError.innerHTML = "Ingrese un DNI válido");
     }
-  }
-  if (error) alert(error);
-}
+  },
+};
+
+formName.addEventListener("blur", errorFunctions.nameValidator);
+formEmail.addEventListener("blur", errorFunctions.emailValidator);
+formPassword.addEventListener("blur", errorFunctions.passwordValidator);
+formAge.addEventListener("blur", errorFunctions.ageValidator);
+formTelephone.addEventListener("blur", errorFunctions.telephoneValidator);
+formAddress.addEventListener("blur", errorFunctions.addressValidator);
+formCity.addEventListener("blur", errorFunctions.cityValidator);
+formZipCode.addEventListener("blur", errorFunctions.zipCodeValidator);
+formDni.addEventListener("blur", errorFunctions.dniValidator);
+submitButton.addEventListener("click", submitForm);
+
+formName.addEventListener("focus", formCorrect); //focused by clicking or pressing tab
+formEmail.addEventListener("focus", formCorrect);
+formPassword.addEventListener("focus", formCorrect);
+formAge.addEventListener("focus", formCorrect);
+formTelephone.addEventListener("focus", formCorrect);
+formAddress.addEventListener("focus", formCorrect);
+formCity.addEventListener("focus", formCorrect);
+formZipCode.addEventListener("focus", formCorrect);
+formDni.addEventListener("focus", formCorrect);
+
+modalClose.addEventListener("click", () => (modalSign.className = "hidden"));
+formName.addEventListener("input", () => (formWelcomeSign.innerHTML = "HOLA " + formName.value));
 
 function formCorrect(e) {
   if (e.target.id === "name") {
@@ -69,7 +127,7 @@ function formCorrect(e) {
   }
 }
 function numAndCharValidator(string) {
-  //analiza si HAY error
+  //Analizes if there IS an error
   let countN = 0;
   let countC = 0;
   for (let char of string) {
@@ -85,7 +143,7 @@ function numAndCharValidator(string) {
   return true;
 }
 function specialCharValidator(string, includeOrExclude, ...specialChar) {
-  //analiza si HAY error
+  //Analize if there IS an error
   let count = 0;
   for (let n of specialChar) {
     if (string.indexOf(n) !== -1) {
@@ -100,73 +158,39 @@ function specialCharValidator(string, includeOrExclude, ...specialChar) {
   }
   return false;
 }
-let formName = document.getElementById("name");
-let formEmail = document.getElementById("email");
-let formPassword = document.getElementById("password");
-let formAge = document.getElementById("age");
-let formTelephone = document.getElementById("telephone");
-let formAddress = document.getElementById("address");
-let formCity = document.getElementById("city");
-let formZipCode = document.getElementById("zip-code");
-let formDni = document.getElementById("dni");
-
-let nameError = document.querySelector(".name-error");
-let emailError = document.querySelector(".email-error");
-let passwordError = document.querySelector(".password-error");
-let ageError = document.querySelector(".age-error");
-let telephoneError = document.querySelector(".tel-error");
-let addressError = document.querySelector(".address-error");
-let cityError = document.querySelector(".city-error");
-let zipCodeError = document.querySelector(".zip-code-error");
-let dniError = document.querySelector(".dni-error");
-
-formName.addEventListener("blur", formError);
-formEmail.addEventListener("blur", formError);
-formPassword.addEventListener("blur", formError);
-formAge.addEventListener("blur", formError);
-formTelephone.addEventListener("blur", formError);
-formAddress.addEventListener("blur", formError);
-formCity.addEventListener("blur", formError);
-formZipCode.addEventListener("blur", formError);
-formDni.addEventListener("blur", formError);
-
-formName.addEventListener("focus", formCorrect); //focused by clicking or pressing tab
-formEmail.addEventListener("focus", formCorrect);
-formPassword.addEventListener("focus", formCorrect);
-formAge.addEventListener("focus", formCorrect);
-formTelephone.addEventListener("focus", formCorrect);
-formAddress.addEventListener("focus", formCorrect);
-formCity.addEventListener("focus", formCorrect);
-formZipCode.addEventListener("focus", formCorrect);
-formDni.addEventListener("focus", formCorrect);
-
-let formWelcomeSign = document.getElementById("welcome-sign");
-formName.addEventListener("input", () => (formWelcomeSign.innerHTML = "HOLA " + formName.value));
-
-let modalSign = document.getElementById("modal");
-let modalText = document.getElementById("modal-text");
-let modalClose = document.getElementById("close-button");
-modalClose.addEventListener("click", () => (modalSign.className = "hidden"));
-
+function validateEverything() {
+  let count = 0;
+  let error = "";
+  for (let element in errorFunctions) {
+    let currentValue = errorFunctions[element]({ target: { value: formInputs[count].value } }); //simulates a blur event
+    if (currentValue) error = error + "\n" + currentValue; // if no error in any input field, error = ''
+    count++;
+  }
+  console.log(error);
+  return error;
+}
 let url =
   "https://curso-dev-2021.herokuapp.com/newsletter?name=Guido+Glielmi&password=guidoglielmi123&telephone=123456789&city=rosario&dni=12345678&email=guido@glielmi.com&age=26&address=lavalle+1234&zip-code=2000";
-let submitButton = document.getElementById("submit");
-submitButton.addEventListener("click", submitForm);
 function submitForm() {
-  fetch(url)
-    .then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        throw Error(response.status + ": " + response.statusText);
-      }
-      return response.json();
-    })
-    .then((json) => {
-      modalText.innerText = JSON.stringify(json);
-      modalSign.className = "modal";
-      for (let key in json) localStorage.setItem(key, json[key]);
-    })
-    .catch((error) => {
-      modalText.innerText = error;
-      modalSign.className = "modal";
-    });
+  if (!validateEverything()) {
+    fetch(url)
+      .then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+          throw Error(response.status + ": " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        modalText.innerText = JSON.stringify(json);
+        modalSign.className = "modal";
+        for (let key in json) localStorage.setItem(key, json[key]);
+      })
+      .catch((error) => {
+        modalText.innerText = error;
+        modalSign.className = "modal";
+      });
+  } else {
+    modalText.innerText = validateEverything();
+    modalSign.className = "modal";
+  }
 }
